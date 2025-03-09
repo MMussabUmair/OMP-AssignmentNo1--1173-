35b9663565c7b60a9bc095ae47a623a5c543de5b
MMussabUmair <22ntucs1173mmussabumair@gmail.com>
2025-03-09 16:22:02 +0500

    Sequntial code of Counting the Even Numbers in the large array
    My Understanding about the code: 
                                                              It’s a sequential code that are used to find the even numbers in a large array very easily in which we use loops and take 1000000 random numbers  between 0 to 999 with the help of rand function and give us the total count of even numbers that are in the array make with the random numbers and we also use clock function that give us the execution time of program. 
    
    If we uncomment the following lines in the above code:
    printf("%d ", numbers[i]);
    printf("\n");
    The program will print all 1,000,000 random numbers generated before counting the even numbers. This will make the output very long because it prints one million numbers in a single line. Writing one million numbers to the screen will significantly increase execution time.  
    
    The Average Execution Time are as follows:
    Sum = 0.006941 + 0.008186 + 0.006770 + 0.006682 + 0.006603 + 0.006640 + 0.007330 + 0.007980 + 0.007172 + 0.008075
    Sum = 0.072379
    Average = sum / 10
    Average = 0.072379 / 10 
    Average = 0.007238

---
+++ Q1.c
@@ -0,0 +1,52 @@
+//Sequntial
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define N 1000000 // define the array size
+
+// function to count the even numbers in the array
+
+int countEvenNumbers(int arr[], int size){
+    int count = 0;
+    for (int i=0; i<size; i++ ){
+        if(arr[i]%2==0){
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main(){
+
+    int numbers[N];
+    srand(time(0));   // random number genetor
+
+    // generate random numbers and print them 
+    printf("generated random numbers:\n");
+    for(int i=0; i<N; i++){
+        numbers[i] = rand() % 1000;
+     //  printf("%d ", numbers[i]);
+
+    }
+
+     // printf("\n");
+
+    //measure execution time of even number counting 
+    clock_t start = clock();
+    int evenCount = countEvenNumbers(numbers, N);
+    clock_t end = clock();
+
+    //print results 
+    printf("Even Numbers Count: %d\n", evenCount);
+    printf("Execution Time: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    return 0;
+}
+
+//Sum = 0.006941 + 0.008186 + 0.006770 + 0.006682 + 0.006603 + 0.006640 + 0.007330 + 0.007980 + 0.007172 + 0.008075
+//Sum = 0.072379
+//Average = sum / 10
+//Average = 0.007238
