60dfad8f67283fccc04ec227b10c9e530d53eb19
MMussabUmair <22ntucs1173mmussabumair@gmail.com>
2025-03-22 21:00:07 +0500

    Parallel(Reduction, Static Scheduling with 4 threads) with chunk size 400
    The parallel implementation of counting even numbers in a large array using OpenMP effectively utilizes static scheduling with a chunk size of 400 and reduction to aggregate results across multiple threads. By setting the number of threads to 4, the workload is evenly distributed, ensuring that each thread processes an equal number of elements, which minimizes thread imbalance. The execution time for 10 runs was recorded, and the average execution time was 0.004574 seconds, demonstrating efficient parallel performance.  However, the low average execution time suggests that the parallel approach significantly optimizes the even number counting task compared to a sequential implementation.

---
+++ Q11.c
@@ -0,0 +1,57 @@
+// Parallel(Reduction, Static Scheduling with 4 threads) with chunk size 400
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<omp.h>
+
+#define N 1000000 // define the array size
+
+// function to count the even numbers in the array
+
+int countEvenParallel (int arr[], int size){
+    int count = 0;
+
+    #pragma omp parallel for reduction(+:count) schedule(static, 400)
+    for (int i=0; i<size; i++ ){
+        if(arr[i] % 2 == 0){
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main(){
+
+    int numbers[N];
+    srand(time(0));   // random number generator
+
+    // generate random numbers and print them 
+    printf("generated random numbers:\n");
+    for(int i=0; i<N; i++){
+        numbers[i] = rand() % 1000;
+      // printf("%d ", numbers[i]);
+
+    }
+
+    //  printf("\n");
+
+    // Set number of threads to 4
+    omp_set_num_threads(4);
+
+    //measure execution time 
+    double start = omp_get_wtime();
+    int evenCount = countEvenParallel(numbers, N);
+    double end = omp_get_wtime();
+
+    //print results 
+    printf("Parallel Even Count: %d\n", evenCount);
+    printf("Execution Time: %f seconds\n", end - start);
+
+    return 0;
+}
+
+//Sum=0.003776+0.004390+0.003389+0.005863+0.004087+0.008791+0.004287+0.004118+0.003665+ 0.003373
+//Average = sum / 10
+//Average = 0.004574
